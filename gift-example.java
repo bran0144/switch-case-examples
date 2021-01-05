fbd54feefb3bd82f428ae374eeb0a44486b61d37
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-01-05 16:16:55 -0600

    learning about switch statements

---
+++ gift-example.java
@@ -0,0 +1,31 @@
+// Each case integer has a specific gift and break statement
+// Default case for those years not listed
+
+String gift;
+switch(year) {
+    case 1:  gift = "Paper"; break;
+    case 2:  gift = "Cotton"; break;
+    case 3:  gift = "Leather"; break;
+    case 4:  gift = "Fruit"; break;
+    case 5:  gift = "Wood"; break;
+    case 6:  gift = "Candy / Iron"; break;
+    case 7:  gift = "Wool / Copper"; break;
+    case 8:  gift = "Bronze / Pottery"; break;
+    case 9:  gift = "Pottery / Willow"; break;
+    case 10: gift = "Tin / Aluminum"; break;
+    case 11: gift = "Steel"; break;
+    case 12: gift = "Silk / Linen"; break;
+    case 13: gift = "Lace"; break;
+    case 14: gift = "Ivory"; break;
+    case 15: gift = "Crystal"; break;
+    case 20: gift = "China"; break;
+    case 25: gift = "Silver"; break;
+    case 30: gift = "Pearl"; break;
+    case 35: gift = "Coral"; break;
+    case 40: gift = "Ruby"; break;
+    case 45: gift = "Sapphire"; break;
+    case 50: gift = "Gold"; break;
+    case 55: gift = "Emerald"; break;
+    case 60: gift = "Diamond"; break;
+    default: gift = "No traditional gift"; break;
+}
